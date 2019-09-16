f7fb55543acfe03485d6a5b8566a03cf59d16620
Carlton <carlton@linux.com>
2019-09-16 22:45:24 +0100

    Initial commit

---
+++ cocks/utils.py
@@ -0,0 +1,31 @@
+import gmpy2
+
+def hex_to_mpz(h):
+    return gmpy2.mpz(int(h, 16))
+
+def str_to_mpz(s):
+    return hex_to_mpz(s.encode().hex())
+
+def mpz_to_hex(m):
+    return hex(m)[2:]
+
+def mpz_to_str(m):
+    return bytes.fromhex(mpz_to_hex(m)).decode()
+
+def hash_mpz(a, f):
+    a = mpz_to_hex(a)
+    a = f(bytes(a, "utf-8")).hexdigest()
+    a = hex_to_mpz(a)
+    return a
+
+class DecryptionFailure(Exception):
+    pass
+
+class ExtractFailure(Exception):
+    pass
+
+class InvalidMessageType(Exception):
+    pass
+
+class InvalidIdentityString(Exception):
+    pass
